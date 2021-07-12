f31147608f7c2ac0c18db1bf370a3a17b639099a
Sean Meade <seanpatmeade@gmail.com>
2021-07-12 16:56:00 +0100

    added resizing with wit dat.gui

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 import { OrbitControls } from 'https://cdn.jsdelivr.net/npm/three@0.121.1/examples/jsm/controls/OrbitControls.js';
+
 let scene, camera, renderer, cube, controls;
 
 // function to keep everything dynamic
@@ -45,6 +46,15 @@ function init() {
   camera.position.z = 5;
 
   controls = new OrbitControls(camera, renderer.domElement)
+
+  var gui = new dat.GUI();
+
+  var box = gui.addFolder('Cube');
+  box.add(cube.scale, 'x', 0, 3).name('Width').listen();
+  box.add(cube.scale, 'y', 0, 3).name('Height').listen();
+  box.add(cube.scale, 'z', 0, 3).name('Length').listen();
+  box.add(cube.material, 'wireframe').listen();
+  box.open();
 
 }
 
