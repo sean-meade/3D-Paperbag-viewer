b32b70ec7f3d3811cd79c383e1b04a4744473642
Sean Meade <seanpatmeade@gmail.com>
2021-07-12 13:47:53 +0100

    Added scene and camera and rendered that to the html page.

--- main.js
+++ main.js
@@ -1,10 +1,20 @@
 // Create Scene
 const scene = new THREE.Scene();
 
+// Create Camera
 const camera = new THREE.PerspectiveCamera(
   75, 
   window.innerWidth/window.innerHeight, 
   0.1, 
   1000
   );
+
+// Create WebGL renderer
+const renderer = new THREE.WebGLRenderer();
+
+// Set size of renderer
+renderer.setSize(window.innerWidth, window.innerHeight);
+
+// Add renderer to body of website
+document.body.appendChild(renderer.domElement)
 
