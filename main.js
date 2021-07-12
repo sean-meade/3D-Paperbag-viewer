f6df6f5caa2d5c2158898016dc3a50be22a03fd0
Sean Meade <seanpatmeade@gmail.com>
2021-07-12 14:14:20 +0100

    Added dynamic resizing in display window.

--- main.js
+++ main.js
@@ -1,44 +1,70 @@
-// Create Scene
-const scene = new THREE.Scene();
+let scene, canera, renderer, cube;
 
-// Create Camera
-const camera = new THREE.PerspectiveCamera(
-  75, 
-  window.innerWidth/window.innerHeight, 
-  0.1, 
-  1000
-  );
+// function to keep everything dynamic
+function init() {
+  // Create Scene
+  scene = new THREE.Scene();
 
-// Create WebGL renderer
-const renderer = new THREE.WebGLRenderer();
+  // Create Camera
+  camera = new THREE.PerspectiveCamera(
+    75, 
+    window.innerWidth/window.innerHeight, 
+    0.1, 
+    1000
+    );
 
-// Set size of renderer
-renderer.setSize(window.innerWidth, window.innerHeight);
+  // Create WebGL renderer
+  renderer = new THREE.WebGLRenderer({
+    // sharpen image
+    antialias: true
+  });
 
-// Add renderer to body of website
-document.body.appendChild(renderer.domElement)
+  // Set size of renderer
+  renderer.setSize(window.innerWidth, window.innerHeight);
 
-// Create Geometry of box
-const geometry = new THREE.BoxGeometry(1, 1, 1);
+  // Add renderer to body of website
+  document.body.appendChild(renderer.domElement)
 
-// Create Material 
-const material = new THREE.MeshBasicMaterial( {color: 0xc19a6c});
+  // Create Geometry of box
+  const geometry = new THREE.BoxGeometry(1, 1.2, 0.5);
 
-// Create object cube (mesh)
-const cube = new THREE.Mesh(geometry, material);
+  // Create Material 
+  const material = new THREE.MeshBasicMaterial( {color: 0xc19a6c});
 
-// add object to scene
-scene.add(cube);
+  // Create object cube (mesh)
+  cube = new THREE.Mesh(geometry, material);
 
-// reposition the camera
-camera.position.z = 5;
+  // add object to scene
+  scene.add(cube);
+
+  // reposition the camera
+  camera.position.z = 5;
+
+}
 
 // Create loop to display object
 function animate() {
   requestAnimationFrame(animate);
 
+  // rotate object
+  cube.rotation.y += 0.01;
+
   // render it all together
   renderer.render(scene, camera);
 }
 
+// Create function to resize everything on window resize
+function onWindowResize() {
+  // reset aspect ratio
+  camera.aspect = window.innerWidth/window.innerHeight;
+  // Update camera
+  camera.updateProjectionMatrix();
+  // reset renderer size
+  renderer.setSize(window.innerWidth, window.innerHeight);
+}
+
+// add event listener for resize to call onWindowResize
+window.addEventListener('resize', onWindowResize, false);
+
+init();
 animate();
