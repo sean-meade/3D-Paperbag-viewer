28aeb8c85f4b7422dab0edf565e1bb2d65621954
Sean Meade <seanpatmeade@gmail.com>
2021-07-12 16:17:25 +0100

    Found out about OrbitControls and realize all my work to get rotation working could be done with two lines of code ... fantastic. Zoom working now too.

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
-let scene, canera, renderer, cube;
+import { OrbitControls } from 'https://cdn.jsdelivr.net/npm/three@0.121.1/examples/jsm/controls/OrbitControls.js';
+let scene, camera, renderer, cube, controls;
 
 // function to keep everything dynamic
 function init() {
@@ -28,6 +29,7 @@ function init() {
   // Create Geometry of box
   const geometry = new THREE.BoxGeometry(1, 1.2, 0.5);
 
+
   // Create Material 
   // const material = new THREE.MeshBasicMaterial( {color: 0xc19a6c});
   const texture = new THREE.TextureLoader().load('textures/paperbag.png');
@@ -42,53 +44,55 @@ function init() {
   // reposition the camera
   camera.position.z = 5;
 
+  controls = new OrbitControls(camera, renderer.domElement)
+
 }
 
-let isRotating = false;
-let mouseX = 0;
-let mouseY = 0;
-let targetX = 0;
-let targetY = 0;
-// use the center of the screen coords
-const windowX = window.innerWidth / 2;
-const windowY = window.innerHeight / 2;
+// let isRotating = false;
+// let mouseX = 0;
+// let mouseY = 0;
+// let targetX = 0;
+// let targetY = 0;
+// // use the center of the screen coords
+// const windowX = window.innerWidth / 2;
+// const windowY = window.innerHeight / 2;
 
-const grabObject = window;
+// const grabObject = window;
 
-// event.offsetX, event.offsetY gives the (x,y) offset from the edge of the canvas.
+// // event.offsetX, event.offsetY gives the (x,y) offset from the edge of the canvas.
 
-// Add the event listeners for mousedown, mousemove, and mouseup
-grabObject.addEventListener('mousedown', e => {
-  mouseX = (e.clientY - windowX);
-  mouseY = (e.clientX - windowY);
-  isRotating = true;
-});
+// // Add the event listeners for mousedown, mousemove, and mouseup
+// grabObject.addEventListener('mousedown', e => {
+//   mouseX = (e.clientY - windowX);
+//   mouseY = (e.clientX - windowY);
+//   isRotating = true;
+// });
 
-grabObject.addEventListener('mousemove', e => {
-  if (isRotating === true) {
-    rotate(mouseX, mouseY);
-    mouseX = (e.clientY - windowX);
-    mouseY = (e.clientX - windowY);
-  }
-});
+// grabObject.addEventListener('mousemove', e => {
+//   if (isRotating === true) {
+//     rotate(mouseX, mouseY);
+//     mouseX = (e.clientY - windowX);
+//     mouseY = (e.clientX - windowY);
+//   }
+// });
 
-window.addEventListener('mouseup', e => {
-  if (isRotating === true) {
-    rotate(mouseX, mouseY);
-    mouseX = 0;
-    mouseY = 0;
-    isRotating = false;
-  }
-});
+// window.addEventListener('mouseup', e => {
+//   if (isRotating === true) {
+//     rotate(mouseX, mouseY);
+//     mouseX = 0;
+//     mouseY = 0;
+//     isRotating = false;
+//   }
+// });
 
-function rotate(mouseX, mouseY) {
+// function rotate(mouseX, mouseY) {
 
-  targetX = mouseX * .01;
-  targetY = mouseY * .01;
+//   targetX = mouseX * .01;
+//   targetY = mouseY * .01;
 
-  cube.rotation.x += 0.05 * (targetX - cube.rotation.x);
-  cube.rotation.y -= 0.05 * (targetY + cube.rotation.y);
-}
+//   cube.rotation.x += 0.05 * (targetX - cube.rotation.x);
+//   cube.rotation.y -= 0.05 * (targetY + cube.rotation.y);
+// }
 
 
 
